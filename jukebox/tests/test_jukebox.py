43610722474dcf1bb2f5372846df0e71f80e65c2
Jorge <me@juniorjorge.com>
2022-10-30 15:53:21 -0400

    Add User class
    Also fix up the tests because writing the first test is always wonky.

--- jukebox/tests/test_jukebox.py
+++ jukebox/tests/test_jukebox.py
@@ -1,6 +1,19 @@
-from jukebox import User
+from jukebox.main import User
+import pytest
+
+
+# Arrange
+@pytest.fixture
+def user():
+    return User("John Doe")
+
 
 class TestUser:
-    def user_has_name(self):
-        user = User("John Doe")
-        assert hasattr(user, name)
+    def test_user_has_name(self, user):
+        # Assert
+        assert hasattr(user, "name")
+
+
+    def test_user_has_credits(self, user):
+        # Assert
+        assert hasattr(user, "credits")
